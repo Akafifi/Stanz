540d961382ee6f0ab15cca16db3fcb65fcffede8
Adam Olson <adam.lee.olson@outlook.com>
2023-11-07 20:54:19 -0600

    fixed fetch request to use segementName

--- Stanz/src/App.jsx
+++ Stanz/src/App.jsx
@@ -4,19 +4,19 @@ function App() {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    // Replace 'YOUR_API_KEY' with your actual Ticketmaster API key
-    const apiKey = 'sAhXfNPP7Dsqx1w9AFN1cjEu9BEDeqNK'; // Use your Ticketmaster Consumer Key
-    const apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=${apiKey}`;
+    const apiKey = 'sAhXfNPP7Dsqx1w9AFN1cjEu9BEDeqNK'; 
+    const apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?keyword=lukecombs&segmentName=Music&apikey=${apiKey}`;
 
     fetch(apiUrl)
       .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Did not work');
         }
         return response.json();
       })
       .then((data) => {
-        setEvents(data._embedded.events);
+        // setEvents(data._embedded.events);
+        console.log('data =', JSON.stringify(data,null,2))
       })
       .catch((error) => {
         console.error('Fetch error:', error);
@@ -27,9 +27,14 @@ function App() {
     <div className="event-list">
       <h1>Ticketmaster Events</h1>
       <ul>
-        {events.map((event) => (
-          <li key={event.id}>{event.name}</li>
-        ))}
+        {/* {events.map((event) => (
+          <li key={event.id}>
+            <strong>{event.name}</strong>
+            <p>Date: {event.dates.start.localDate}</p> 
+            <p>Location: {event._embedded.venues[0].name}</p> {location}
+            
+          </li>
+        ))} */}
       </ul>
     </div>
   );
