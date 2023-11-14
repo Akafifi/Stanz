4530f1460951050f05cbaa89f6a3b61abbadbc3d
noahr33 <128255532+noahr33@users.noreply.github.com>
2023-11-13 21:23:48 -0600

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ import {
 } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 import { Outlet } from 'react-router-dom'
-// import 'bootstrap/dist/css/bootstrap/min.css'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
