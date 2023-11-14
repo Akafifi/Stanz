a29e9861cba7a64abb2af43b46cf8ff58a20c6f6
Adam Olson <adam.lee.olson@outlook.com>
2023-11-13 20:52:05 -0600

    updating geolocation data

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,10 +3,74 @@ import { useQuery } from '@apollo/client';
 import ProfileList from '../components/ProfileList';
 
 import { QUERY_PROFILES } from '../utils/queries';
+import GoogleMapReact from 'google-map-react';
+import MapPin from '../components/mapPin'; 
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_PROFILES);
   const profiles = data?.profiles || [];
+
+  const defaultProps = {
+    center: {
+      lat: 44.500000,
+      lng: -89.500000,
+    },
+    zoom: 11
+  };
+  var testArray=
+  [
+    {
+      venue: 'State Farm Stadium',
+      dateTime: '2024-06-02T00:45:00Z',
+      city: 'Glendale',
+      state: 'AZ',
+      geoPoint: { lat: '33.5312', long: '-112.2202' }
+    },
+    {
+      venue: 'State Farm Stadium',
+      dateTime: '2024-06-01T00:45:00Z',
+      city: 'Glendale',
+      state: 'AZ',
+      geoPoint: { lat: '33.5312', long: '-112.2202' }
+    },
+    {
+      venue: 'NRG Stadium',
+      dateTime: '2024-08-10T22:45:00Z',
+      city: 'Houston',
+      state: 'TX',
+      geoPoint: { lat: '29.6835', long: '-95.400101' }
+    },
+    {
+      venue: 'FedEx Field',
+      dateTime: '2024-07-27T21:45:00Z',
+      city: 'Landover',
+      state: 'MD',
+      geoPoint: { lat: '38.918301', long: '-76.872101' }
+    },
+    {
+      venue: 'NRG Stadium',
+      dateTime: '2024-08-09T22:45:00Z',
+      city: 'Houston',
+      state: 'TX',
+      geoPoint: { lat: '29.6835', long: '-95.400101' }
+    },
+    {
+      venue: 'FedEx Field',
+      dateTime: '2024-07-26T21:45:00Z',
+      city: 'Landover',
+      state: 'MD',
+      geoPoint: { lat: '38.918301', long: '-76.872101' }
+    },
+    {
+      venue: 'FedEx Field',
+      dateTime: undefined,
+      city: 'Landover',
+      state: 'MD',
+      geoPoint: { lat: '38.918301', long: '-76.872101' }
+    }
+  ]
+
+
 
   return (
     <main>
@@ -22,6 +86,25 @@ const Home = () => {
           )}
         </div>
       </div>
+
+      <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+       {testArray.map(stop=> {
+        return <MapPin stop={stop} lat= {
+          stop.geoPoint.lat
+        }
+        lon= {
+          stop.geoPoint.long
+        }/>
+       })} 
+     
+      </GoogleMapReact>
+    </div>
+
     </main>
   );
 };
